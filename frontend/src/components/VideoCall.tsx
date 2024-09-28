e318445c9c1cbe8e460860a72cf23922ce5afec8
Aries Dmello <dmelloaries@gmail.com>
2024-09-29 03:11:17 +0530

    Testing2

--- frontend/src/components/VideoCall.tsx
+++ frontend/src/components/VideoCall.tsx
@@ -652,7 +652,8 @@ const VideoCall: React.FC = () => {
             handleToggleVideo={() => {}} // Add empty function to satisfy prop types
           />
         </div>
-        <Code clickedIcon={clickedIcon} user={user?.fullName || ""} />
+        // @ts-ignore
+        <Code clickedIcon={clickedIcon} user={user?.fullName || ""} /> 
       </div>
       
 
